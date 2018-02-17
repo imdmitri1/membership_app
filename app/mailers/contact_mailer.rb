06ab0f3d7557e5beb74669cc4f214ff7334ffe70
Dmitri Stefanisin <email.ds@protonmail.com>
2018-02-16 20:29:46 -0600

    add contact mailer model

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,12 @@
+class ContactMailer < ActionMailer::Base
+  default to: email.ds@protonmail.com
+
+  def contact_email(name, email, body)
+    @name = name
+    @email = email
+    @body = body
+
+    mail(from: email, subject: "Contact from MembershipApp")
+  end
+
+end 
