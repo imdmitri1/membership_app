f107ec6b4bc5e2b87240525ab45d5bf19cdc0f63
Dmitri Stefanisin <email.ds@protonmail.com>
2018-02-16 20:03:13 -0600

    trying to fix db validators, still not working

--- db/migrate/20180216225221_create_contacts.rb
+++ db/migrate/20180216225221_create_contacts.rb
@@ -1,9 +1,9 @@
 class CreateContacts < ActiveRecord::Migration[5.1]
   def change
     create_table :contacts do |t|
-      t.string :name, null: false, limit: 64
-      t.string :email, null: false, limit: 64
-      t.text   :comments, null: false, limit: 1024
+      t.string :name, null: false, length: 3..64 #, limit: 64 #this validations aren't working!
+      t.string :email, null: false, length: 3..64 #, limit: 64 #this validations aren't working!
+      t.text   :comments, null: false, length: 3..1024 #, limit: 1024 #this validations aren't working!
 
       t.timestamps
     end
