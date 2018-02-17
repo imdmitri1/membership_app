83096d0e263341a6b4219f2193abf33b78ba13cf
Dmitri Stefanisin <email.ds@protonmail.com>
2018-02-16 20:30:33 -0600

    add contact mailer logic in contacts_controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,6 +7,10 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.create(contact_params)
     if @contact.save
+      name = contact_params[:contact][:name]
+      email = contact_params[:contact][:email]
+      body = contact_params[:contact][:comments]
+      ContactMailer.contact_email(name, email, body).deliver
       flash[:success] = "Message sent!"
       redirect_to new_contact_path
     else
